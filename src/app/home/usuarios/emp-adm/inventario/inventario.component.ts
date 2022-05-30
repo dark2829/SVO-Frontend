8c7ba1e35c02d1a3cc007e1551b2c4a2e43f36f9
dark2829 <tozcuento.2829@gmail.com>
2022-05-30 02:07:19 -0500

    visualizar producto en pequeño

--- src/app/home/usuarios/emp-adm/inventario/inventario.component.ts
+++ src/app/home/usuarios/emp-adm/inventario/inventario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { ProductosService } from '../../../../services/productos.service';
 
@@ -10,11 +11,13 @@ import { ProductosService } from '../../../../services/productos.service';
 export class InventarioComponent implements OnInit {
   
   productos: any = {};
+  infoProduct: any = null;
   public page: number = 1; 
 
   constructor(
     private router: Router,
-    private producto: ProductosService
+    private producto: ProductosService,
+    private sanitizer: DomSanitizer 
   ) { }
 
   ngOnInit(): void {
@@ -23,12 +26,25 @@ export class InventarioComponent implements OnInit {
     });
   }
 
+  showProduct(producto: any){
+    this.infoProduct = producto; 
+  }
+
   registro(){
     this.router.navigate(['product-register'])
   }
 
   modify(){
     this.router.navigate(['product-modify'])
+  }
+
+  regresarImg(b64: string){
+    if(b64 == null){
+      return null; 
+    }else{
+      return this.sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' 
+      + `${b64}`);
+    }
   }
 
 }
