b59575342cfb6dd49815bd387d4bd00e61841eb2
dark2829 <tozcuento.2829@gmail.com>
2022-05-31 20:19:20 -0500

    modificaicon de carrito de compras

--- src/app/home/usuarios/cliente/shopping-cart/shopping-cart.component.ts
+++ src/app/home/usuarios/cliente/shopping-cart/shopping-cart.component.ts
@@ -122,7 +122,7 @@ export class ShoppingCartComponent implements OnInit {
           });
         }
       }else{
-        console.log("No hay productos en carrito");
+        // console.log("No hay productos en carrito");
       }
     }
     );
